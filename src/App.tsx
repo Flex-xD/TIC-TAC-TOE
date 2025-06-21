64da46146226237f6e7226bed1b1e15983713332
Flex-xD <bishtaman88102@gmail.com>
2025-06-21 15:08:47 +0530

    Winner is getting declared and winning logic is working but only when all the boxes are being filled

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import { Box } from "./Box";
 const App = () => {
 
   const [values, setValues] = useState(Array(9).fill(undefined));
-  const [nextMoveX , setNextMoveX] = useState(true);
+  const [nextMoveX, setNextMoveX] = useState(true);
+  const [winner, setWinner] = useState("");
 
   const handleClick = (index: number) => {
-    console.log(index, " ,  Box Clicked !")
     const updatedValues = [...values];
+    if (updatedValues[index] !== undefined) {
+      return
+    }
+    console.log(index, " ,  Box Clicked !")
     if (nextMoveX) {
       updatedValues[index] = "X"
       setNextMoveX(false)
@@ -17,13 +21,39 @@ const App = () => {
       setNextMoveX(true)
     }
     setValues(updatedValues)
+    winnerDeclaration()
     console.log(updatedValues)
     return updatedValues;
   }
 
+  const winningCombinaion = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  const winnerDeclaration = () => {
+    for (let i = 0; i < winningCombinaion.length; i++) {
+      const [a, b, c] = winningCombinaion[i];
+      if (values[a] === values[b] && values[a] === values[c]) {
+        console.log("The winner is " , values[a])
+        setWinner(values[a])
+      }
+    }
+  }
+
 
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-gray-500">
+    <div className="h-screen w-screen flex flex-col items-center justify-evenly bg-gray-500">
+      <h1 className="text-4xl font-black text-gray-700 font-stretch-90%">{
+        winner ? `The winner is ${winner}` : "GAME STARTS NOW !"
+        }
+      </h1>
       <div className="h-[30rem] w-[30rem] bg-gray-500 rounded-lg shadow-2xl grid grid-cols-3 grid-rows-3 gap-2">
         <Box identity={0} value={values[0]} handleClick={() => handleClick(0)} />
         <Box identity={1} value={values[1]} handleClick={() => handleClick(1)} />
--- src/Box.tsx
+++ src/Box.tsx
@@ -2,7 +2,7 @@
 export const Box = ({ identity, value, handleClick }: { identity: number, value: any, handleClick: (index: number) => void }) => {
 
     return (
-        <div className="bg-gray-400 h-[10rem] w-[10rem] border-1 rounded-lg flex items-center justify-center text-9xl font-bold" onClick={() => handleClick(identity)}>
+        <div className="bg-gray-500 h-[10rem] w-[10rem] rounded-lg flex items-center justify-center text-9xl font-bold shadow-2xl" onClick={() => handleClick(identity)}>
             {value}
         </div>
     )
